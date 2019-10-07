2b09314984d6876c640df03e2f9268f429a150c1
佐藤誠也 <satohseiya@satouseiyanoMacBook-Pro.local>
2019-10-08 07:43:48 +0900

    add feature of user registration

--- Gemfile
+++ Gemfile
@@ -54,3 +54,4 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'rails-controller-testing'
+gem 'bootstrap-sass', '3.3.7'
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root 'static_pages#home'
   #get 'static_pages/home'
   get '/home', to:'static_pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/signup', to:'users_#new'
+   
+  post '/signup', to:'users_#create'
+  resources :users
 end
